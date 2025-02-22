dc92c78a8bf9433fdb29f305d018baba61650f04
unknown <seokangwoo99@naver.com>
2025-02-22 23:45:48 +0800

    error fix 15

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,51 +3,28 @@ import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
 import SearchContent from "./components/SearchContent";
 
-// interface SearchProps {
-//   searchParams: {
-//     title: string;
-//   }
-// };
-
-// const Search = async ({searchParams}: SearchProps) => {
-//   const songs = await getSongsByTitle(searchParams.title);
-
-//   return (
-//     <div className="bg-neutral-900 rounded-lg h-full overflow-hidden overfow-y-auto">
-//       <Header className="from-bg-neutral-900">
-//         <div className="mb-2 flex flex-col gap-y-6">
-//           <h1 className="text-white text-3xl font-semibold">
-//             Search
-//           </h1>
-//           <SearchInput/>
-//         </div>
-//       </Header>
-//       <SearchContent songs={songs}/>
-//     </div>
-//   )
-// };
-
-// export default Search;
-
 interface SearchProps {
-  searchParams: Record<string, string | undefined>;
-}
+  searchParams: {
+    title: string;
+  }
+};
 
-const Search = async ({ searchParams }: SearchProps) => {
-  const title = searchParams.title || ''; // Default to an empty string if undefined
-  const songs = await getSongsByTitle(title);
+const Search = async ({searchParams}: SearchProps) => {
+  const songs = await getSongsByTitle(searchParams.title);
 
   return (
-    <div className="bg-neutral-900 rounded-lg h-full overflow-hidden overflow-y-auto">
+    <div className="bg-neutral-900 rounded-lg h-full overflow-hidden overfow-y-auto">
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
-          <h1 className="text-white text-3xl font-semibold">Search</h1>
-          <SearchInput />
+          <h1 className="text-white text-3xl font-semibold">
+            Search
+          </h1>
+          <SearchInput/>
         </div>
       </Header>
-      <SearchContent songs={songs} />
+      <SearchContent songs={songs}/>
     </div>
-  );
+  )
 };
 
 export default Search;
